5c9e37bfb9998e2d0cbc3899e0397ffddd8e62d6
Miguel <migueljherrera@gmail.com>
2018-05-11 16:08:24 -0300

    feat(binary): add mysqldump and mariabackup binary as asset for golang

--- boot.go
+++ boot.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"flag"
-
-	"github.com/segurosfalabella/imperium-backinator/app"
+    "flag"
+    "fmt"
+    maria "github.com/segurosfalabella/imperium-backinator/binary-maria"
+    mysql "github.com/segurosfalabella/imperium-backinator/binary-mysql"
 )
 
 var endpoint = flag.String("endpoint", "http://consul-v2.tools.segurosfalabella.cloud", "Consul API endpoint")
 var token = flag.String("token", "", "Consul admin token")
 
 func main() {
-	flag.Parse()
-	app.Backup(*endpoint, *token)
+    fmt.Println(maria.AssetNames())
+    fmt.Println(mysql.AssetNames())
+    // flag.Parse()
+    // app.Backup(*endpoint, *token)
 }
