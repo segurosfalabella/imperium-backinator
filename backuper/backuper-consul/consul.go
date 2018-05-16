9fe6107da7e8e7f207bd085fef29909150e9e682
Miguel <migueljherrera@gmail.com>
2018-05-16 11:08:30 -0400

    feat(consul): Try other way with http-client

--- backuper/backuper-consul/consul.go
+++ backuper/backuper-consul/consul.go
@@ -1,63 +1,14 @@
 package consul
 
 import (
-	"bytes"
 	"errors"
-	"fmt"
-	"io"
-	"os"
 	"regexp"
-
-	"github.com/hashicorp/consul/api"
 )
-
-//OsCreate var
-var OsCreate = os.Create
-
-//API var
-var API = api.NewClient
 
 //Backuper struct
 type Backuper struct {
 	Endpoint string
 	Token    string
-}
-
-type CustomClient struct {
-	client *api.Client
-}
-
-func (cc *CustomClient) Snapshot() *MySnapshot {
-	return &MySnapshot{
-		snapshot: &api.Snapshot{},
-	}
-}
-
-//CustomClient interface
-type CustomClientInterface interface {
-	Snapshot() *MySnapshot
-}
-
-type MySnapshot struct {
-	snapshot *api.Snapshot
-}
-
-func (ms *MySnapshot) Snapshot() *MySnapshot {
-	return &MySnapshot{snapshot: &api.Snapshot{}}
-}
-
-func (ms *MySnapshot) Save(options *api.QueryOptions) (io.ReadCloser, *api.QueryMeta, error) {
-	return ms.snapshot.Save(options)
-}
-
-func NewClient(config *api.Config) (CustomClientInterface, error) {
-	client, err := api.NewClient(config)
-	if err != nil {
-		return nil, errors.New("could not upgrade new client method")
-	}
-	CustomClient := new(CustomClient)
-	CustomClient.client = client
-	return CustomClient, nil
 }
 
 // Backup functionality
@@ -70,47 +21,16 @@ func (cb *Backuper) Backup() error {
 		return errors.New("token is required")
 	}
 
-	queryOptions := &api.QueryOptions{
-		Token: cb.Token,
-	}
-
-	config := &api.Config{
-		Address: cb.Endpoint,
-	}
-
-	client, err := API(config)
-	if err != nil {
-		return errors.New("could not create new client")
-	}
-
-	readCloser, _, err := client.Snapshot().Save(queryOptions)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer readCloser.Close()
-
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(readCloser)
-	newStr := buf.String()
-
-	file, err := OsCreate("backup.tgz")
-	if err != nil {
-		return errors.New("could not create backup file")
-	}
-
-	defer file.Close()
-	file.WriteString(newStr)
-
+	// queryOptions := &api.QueryOptions{
+	// 	Token: cb.Token,
+	// }
+	//
+	// config := &api.Config{
+	// 	Address: cb.Endpoint,
+	// }
+	//
+	// fmt
 	return nil
-}
-
-//SaveSnapshot method
-func SaveSnapshot(client CustomClient, options *api.QueryOptions) io.ReadCloser {
-	readCloser, _, err := client.Snapshot().Save(options)
-	if err != nil {
-		return nil
-	}
-	return readCloser
 }
 
 //ValidateEndpoint ...
--- backuper/backuper-consul/consul_test.go
+++ backuper/backuper-consul/consul_test.go
@@ -1,33 +1,11 @@
 package consul_test
 
 import (
-	"errors"
-	"io"
 	"testing"
 
-	"github.com/hashicorp/consul/api"
 	consul "github.com/segurosfalabella/imperium-backinator/backuper/backuper-consul"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
-
-type mockClient struct {
-	mock.Mock
-}
-
-type mockSnapshot struct {
-	c *mockClient
-}
-
-// Snapshot returns a handle that exposes the snapshot endpoints.
-func (c *mockClient) Snapshot() *consul.MySnapshot {
-	return &consul.MySnapshot{}
-}
-
-func (s *mockSnapshot) Save(q *api.QueryOptions) (io.ReadCloser, *api.QueryMeta, error) {
-	err := errors.New("could not save snapshot")
-	return nil, nil, err
-}
 
 func TestShouldReturnErrorWhenEndpointIsNotValid(t *testing.T) {
 	var endpoint = "hola mundo"
@@ -68,61 +46,3 @@ func TestShoudlValidateMethodReturnFalseWhenEndpointIsInvalid(t *testing.T) {
 
 	assert.False(t, state)
 }
-
-func TestShoudlReturnErrorWhenCallNewClientFail(t *testing.T) {
-	var endpoint = "http://www.google.com"
-	var token = "token-demo"
-	consulInstance := &consul.Backuper{
-		Endpoint: endpoint,
-		Token:    token,
-	}
-	var oldApi = consul.API
-	defer func() { consul.API = oldApi }()
-	consul.API = func(config *api.Config) (*api.Client, error) {
-		return nil, errors.New("could not create new client")
-	}
-
-	err := consulInstance.Backup()
-
-	assert.NotNil(t, err)
-	assert.Equal(t, err.Error(), "could not create new client")
-}
-
-// func TestShouldReturnNilWhenSnapshotFail(t *testing.T) {
-// 	var token = "token-demo"
-// 	options := &api.QueryOptions{
-// 		Token: token,
-// 	}
-// 	client := new(mockClient)
-// 	result := consul.SaveSnapshot(client, options)
-//
-// 	assert.NotNil(t, result)
-// }
-
-// func TestShouldReturnErrorWhenSnapshotSaveFail(t *testing.T) {
-// 	var endpoint = "http://www.google.com"
-// 	var token = "token-demo"
-// 	consulInstance := &consul.Backuper{
-// 		Endpoint: endpoint,
-// 		Token:    token,
-// 	}
-//
-// }
-
-// func TestShouldReturnErrorWhenOsCreateFile(t *testing.T) {
-// 	var endpoint = "http://www.google.com"
-// 	var token = "token-string"
-// 	consulInstance := &consul.Backuper{
-// 		Endpoint: endpoint,
-// 		Token:    token,
-// 	}
-// 	var oldFunc = consul.OsCreate
-// 	defer func() { consul.OsCreate = oldFunc }()
-// 	consul.OsCreate = func(name string) (*os.File, error) {
-// 		return nil, errors.New("could not create backup file")
-// 	}
-//
-// 	err := consulInstance.Backup()
-//
-// 	assert.NotNil(t, err)
-// }
